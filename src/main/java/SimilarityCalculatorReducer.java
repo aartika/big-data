1b3f6b003cfd0929618681be6ae717f1fc431dd9
Aartika Rai <aartika.iitd@gmail.com>
2015-11-08 00:55:36 +0530

    minimum number of common ratings

--- src/main/java/SimilarityCalculatorReducer.java
+++ src/main/java/SimilarityCalculatorReducer.java
@@ -1,3 +1,4 @@
+import com.google.common.base.Optional;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -11,28 +12,31 @@ public class SimilarityCalculatorReducer extends Reducer<ProductPair, RatingPair
 
     @Override
     protected void reduce(ProductPair key, Iterable<RatingPair> values, Context context) throws IOException, InterruptedException {
-        context.write(
-                new ProductPair(key.getProductId1(), key.getProductId2()),
-                new DoubleWritable(
-                        cosineSimilarity(values.iterator())
-                )
-        );
+        Optional<Double> similarity = cosineSimilarity(values.iterator());
+        if (similarity.isPresent()) {
+            context.write(
+                    new ProductPair(key.getProductId1(), key.getProductId2()),
+                    new DoubleWritable(similarity.get())
+            );
+        }
+
     }
 
-    private double cosineSimilarity(Iterator<RatingPair> ratingPairIterator) {
+    private Optional<Double> cosineSimilarity(Iterator<RatingPair> ratingPairIterator) {
         double dotProduct = 0.0;
         double sumOfSquares1 = 0.0;
         double sumOfSquares2 = 0.0;
+        int countOfPairs = 0;
 
         while (ratingPairIterator.hasNext()) {
+            countOfPairs++;
             RatingPair pair = ratingPairIterator.next();
             dotProduct += pair.getRating1() * pair.getRating2();
             sumOfSquares1 += pair.getRating1() * pair.getRating1();
             sumOfSquares2 += pair.getRating2() * pair.getRating2();
         }
 
-        return sumOfSquares1 > 0 && sumOfSquares2 > 0 ?
-                dotProduct / (Math.sqrt(sumOfSquares1) * Math.sqrt(sumOfSquares2)) :
-                0.0;
+        return countOfPairs < 5 ? Optional.<Double>absent() :
+                Optional.of(dotProduct / (Math.sqrt(sumOfSquares1) * Math.sqrt(sumOfSquares2)));
     }
 }
